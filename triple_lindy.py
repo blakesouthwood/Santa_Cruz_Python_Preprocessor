3fa055fc334f05d0e0904cc3010701c5165b7dc0
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2020-12-17 19:09:53 -0800

    Add files via upload
    in beta 3 now for switch case for python.
    Works with ifs, will add dictionary mode next
    Still polishing the code

---
+++ triple_lindy.py
@@ -0,0 +1,60 @@
+
+#need input for exp for switch(exp)
+
+switchcasetester = '''
+	switch(exp) {
+		case 'coffee':  #change test input x
+		case 'tea':
+			print(\"where is my mocha!\")
+			print("extra hot with whip")
+			break
+
+		case 'walmart':  #change test input x
+		case 'target':
+			print(\"what a sale today!\")
+			print("everything is on christmas sale")
+			break
+			
+		case 'frosty':  #change test input x
+		case 'rudolph':
+			print(\"so many snow men!\")
+			print("first sled the sleigh ready")
+			break
+
+		case 'candy':
+		case 'coconuts':
+			print("see's candies  section\")
+			
+			
+		case 'fish':
+		case 'marsbar':
+			print('third  section')
+			print('working on this')
+			
+		case 'panera':
+		case 'peanuts':
+			print('four try again')
+		
+		case 'motel':
+		case 'hotel':
+			print('do you have a reservation')
+			break
+			
+
+		case 'alpine':
+		case 'squaw':
+			print('ski fast in the powder')
+			break
+			
+		default:
+			print('six walking duck de fa ul t')
+			print("flying geese")
+			break
+}
+'''
+
+
+
+  
+
+ 
