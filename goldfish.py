5134e58c6f710c2f4c7e60072bb1628592b29e11
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2020-12-24 15:55:44 -0800

    Add files via upload
    testing. I figured out how to access the file with the switch case code. Tricky but it works.
    Now will experiment getting javascript switch case on front page. So pleased right now.
    christmas eve

---
+++ goldfish.py
@@ -0,0 +1,22 @@
+from yosemite_falls import *
+#global switchcasetester
+
+#print(switchcasetester) #whcih is the long string already in yosemite_falls
+
+#budweiser()
+
+#varholder=[]
+#varholder.append("walmart") #these are input for the switch case (unseen)
+#adjust_input("walmart")
+
+clever('squaw') #this would change varholder[0] to fish
+#the switch case tester input switch is in yosemite
+smart() #this calls the parser and codegen
+
+
+
+
+
+
+
+ 
