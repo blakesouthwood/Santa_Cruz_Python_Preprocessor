2a8716d9560cb5d1c95124a28c6f311bdbb1cb97
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2020-12-28 22:29:32 -0800

    Update goldfish.py

--- goldfish.py
+++ goldfish.py
@@ -6,6 +6,20 @@
 #need to add ability of doing numbers in switch
 #finally have momentum
 
+#Note for the switch case notice the indentation used
+# It is based on 4 space tabs
+# 1 tab for switch
+# 2 tabs for case
+# 3 tabs for code inside of case
+# this format of tabs in the switch case is copied and used for the python generated switch
+# so it's important and imperative that you adopt this format until I add auto-indent facilitated with lovely braces { }
+# I completely  abhore the tab indentation rules in Python I think they are pure ballony
+# so in the near future it's pure JavaScript/C  braces with automatic tabs added using fuzzy logic patterns
+# I have wasted so much precious time with mindless indentation errors in my code in Python
+# JavaScript doesn't care about indentation and neither should Python
+# If you notice with this radical switch case in python I'm making a hybrid blend of Python that is
+# going to be way more user-friendly and make sense to true programmers and will result in coding much faster
+
 from switch_mod import *  #this is the switch case module
 
 
