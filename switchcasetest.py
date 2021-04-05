406940d972bd217a19cd2bf151e1672c46f8907b
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2021-04-04 22:12:31 -0700

    Add files via upload
    Comment out one switch and try one and then try the other.
    Numbers and words work now.  I will take out the comment print statements tomorrow
    now that it works. What a bear to debug this. So pleased that it actually works and it's fast.
    It will be faster after I take out the print statements.

---
+++ switchcasetest.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+
+#from snoopy1 import *     # case numbers
+from switchcase_fuzzylogic import *  #normal case words
+
+#from pre_versionworkingfallsjan26th import *
+
+    
+#this is  input variable exp for switch(exp)
+print("===== FIRST 1st switch =====")
+
+clever('Blake') #this would change varholder[0] 
+
+
+
+
+sw = '''
+	switch(exp) {
+		case 'charlie brown':  
+		case 'linus':
+		case 'lucy':
+			print(\"where's the dog house!\")
+			print("first prize")
+			print("you block head Charlie Brown")
+			break
+
+		case 'colin likes ice cream':  
+		case 'blake':
+			print("coding right now")
+			print(" in python")
+			break
+
+		case 'Suez Canal blocked':
+			print("massive bottleneck in Egypt")
+			break
+			
+		case 'thank god':  
+		case 'table':
+			print("lucky to have bbedit")
+			print("=for my backups=")
+			print("----------------------")
+			break
+
+		case 'william pilot':
+			print("jet figher pilot extroideniare\")
+			print("-------------")
+			get('phantom jet')
+			break
+			
+			
+		case 'xmas':
+		case 'newyears':
+			print('christmas')
+			print('new years eve')
+			break
+			
+		case 'panera':
+		case 'peanuts':
+			print('four try again')
+			
+			
+		
+		case 'motel 6 in gilroy':
+		case 'travelcenter':
+			print('nice place to stay')
+			
+			
+
+		case 'Alpine Meadows':
+		case 'squaw':
+			print('ski fast in the powder')
+			
+		
+		
+		case 'squawnow':
+			print('ski fast in the powder')
+			
+			
+		
+		case 'squaw fish':
+			print('ski fast in the powder')
+			
+			
+		
+		case 'snow time':
+			print('ski fast in the powder')
+			
+			
+		
+		case 'have a cow':
+			print('push the cow over before the ufo comes')
+			break
+				
+		default:
+			print('six walking duck de fa ul t')
+			print("flying geese")
+}
+'''
+endswitch(sw)
+
+
+#number test faster just uncomment it and above comment endswitch(sw)
+#sw =
+'''
+	switch(exp) {  
+		case 1 thru 5:
+			print("where\'s the dog house!")
+			print('first prize')
+			print('you block head Charlie Brown')
+			break
+
+		case 6 to 10:
+			print('kangaroo hop hop!')
+			print('taught me how to write code')
+			break
+			
+		 
+		case 11 to 15:
+			print('mocha blast')
+			print('== 31 flavors===')
+			break;
+
+			
+
+		case 16 to 20:
+			print('ski fast in the powder')
+			print('sweet powder snow, lovely snow')
+			break
+			
+		case 21:
+			print('Heavenly valley')
+			print('big snow flakes there and moggles')
+			break
+			
+		default:
+			print('six walking duck de fa ul t')
+			print('flying geese')
+            break
+}
+'''
+#endswitch(sw)
+
