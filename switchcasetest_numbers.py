91f49907b50f0e17e3d109a167ce35d958f73108
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2021-04-29 23:26:07 -0700

    Add files via upload
    The flow valve that determines if a switch is numbers or words is in this file. This one doesn't have the reload module.

---
+++ switchcasetest_numbers.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+
+#from snoopy1 import *     # case numbers
+from switch_module3 import *  #normal case words
+
+#from pre_versionworkingfallsjan26th import *
+
+	
+clever('17')
+	
+sw = '''
+	switch(exp) {  
+		case 1 thru 5:
+			print("where\'s the dog house!")
+			print('first prize')
+			print('you block head Charlie Brown')
+			break
+
+		case 6 to 10:
+			print('kangaroo hop hop!')
+			print('taught me how to write code')
+			break
+			
+		 
+		case 11 to 15:
+			print('mocha blast')
+			print('== 31 flavors===')
+			break
+
+			
+
+		case 16 to 20:
+			print('ski fast in the powder')
+			print('sweet powder snow, lovely snow')
+			break
+			
+		case 21:
+			print('Heavenly valley')
+			print('big snow flakes there and moggles')
+			break
+			
+			
+		case 26:
+			print('oh yeah')
+			print('tahoe here I come')
+			break
+			
+		default:
+			print('six walking duck de fa ul t')
+			print('flying geese')
+            break
+}
+'''
+endswitch(sw)
+
+
+   
