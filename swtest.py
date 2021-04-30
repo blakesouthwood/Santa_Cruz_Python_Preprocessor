9556b22e98889b51184d4cbd77ebaa5222e92851
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2021-04-29 22:59:23 -0700

    Add files via upload
    Still a work in progress but working. Uses reload each time a switch is called (for now).
    I am resetting the vars and lists after each switch but for some reason it crashes on switches after first UNLESS I reload the switch module.
    With the reload it works like a charm. Will take out the print statements now.
    Blake Southwood

---
+++ swtest.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+
+
+#from snoopy1 import *     # case numbers
+#from switch_module3 import *  #normal case words
+#from switch_mod2backup import *  #normal case words
+#from switchmod4gold import *
+import importlib
+import switchmodtrial7
+from switchmodtrial7 import *  #april 29th, 2021 flailing
+#from pre_versionworkingfallsjan26th import *
+
+def sw_reset():
+	importlib.reload(switchmodtrial7)
+
+
+#this is  input variable exp for switch(exp)
+print("===== FIRST 1st switch =====")
+print("===== FIRST 1st switch =====")
+print("===== FIRST 1st switch =====")
+
+clever('rasberries and cream') #this would change varholder[0] 
+
+sw = '''
+	switch(exp) {
+		case 'rasberries and cream':  
+			print(\"where's the cream house!\")
+			print("tastes good")
+			break
+			
+		case 'gone fishing':
+			print('fallen leaf lake')
+			break
+			
+		case 'driving car':
+			print('where is the gti')
+			print("really a porsche...")
+			break
+			
+		case 'macbook pro':
+			print('fast laptop')
+			print("when will the code work...")
+
+		default:
+			print('six walking duck de fa ul t')
+			print("flying geese")
+			break
+}
+'''
+endswitch(sw)
+
+print("========MIDDLE EARTH THERE ======")
+print("========MIDDLE EARTH THERE ======")
+print("========MIDDLE EARTH THERE ======")
+
+print("Printed immediately.")
+
+sw_reset()
+print("let the games begin")
+
+clever('snow time') #this would change varholder[0] 
+
+print("2nd switch -------")
+
+sw ='''
+	switch(exp) {
+		case 'charlie brown':  
+			print(\"where's the dog house!\")
+			print("first prize")
+			print("you block head Charlie Brown")
+			
+	
+		
+		case 'snow time':
+			print('ski fast in the powder')
+			print("the snow at Tahoe is just beautiful")
+			
+		
+		case 'have a cow':
+			print('push the cow over before the ufo comes')
+			print("going up...")
+			print("straight into the clouds...")
+			print("...")
+			
+			
+				
+		default:
+			print('six walking duck de fa ul t')
+			print("flying geese")
+			break
+}
+'''
+endswitch(sw)
+
+
+
+#clever('rasberries and cream') #this would change varholder[0] 
+
+print("=====  3nd switch =====")
+print("=====  3nd switch =====")
+print("=====  3nd switch =====")
+
+
+
+sw_reset()
+print("third times a charm")
+
+clever('ufos are real') #this would change varholder[0] 
+
+
+
+
+sw = '''
+	switch(exp) {
+		case 'fishy':  
+		case 'two da':
+		case 'three da':
+			print(\"squirt gun!\")
+			print("water everywhere")
+			break
+
+			
+		case 'big bird':
+			print("Sesame Street")
+			print(" groucho and animal")
+			print("this is so fun")
+			break
+
+		case 'Israel canal':
+			print("massive ship blocking canal in Egypt")
+			print("it was stuck for six days")
+			
+			
+		case 'ufos are real':
+			print('manure')
+			print("no more horses")
+			break
+		
+				
+		default:
+			print('one walking quack d')
+			print("walking geese")
+			break
+}
+'''
+endswitch(sw)
+
+
+print("========after second switch ends ======")
+print("========after second switch ends  ======")
+#clever('big bird') #this would change varholder[0] 
+
+#this is the code being run
+#sw = 
+'''
+	switch(exp) {
+		case 'fishy':  
+		case 'two da':
+		case 'three da':
+			print(\"squirt gun!\")
+			print("water everywhere")
+			break
+
+			
+		case 'big bird':
+			print("Sesame Street")
+			print(" groucho and animal")
+			print("this is so fun")
+			break
+
+		case 'Israel canal':
+			print("massive ship blocking canal in Egypt")
+			print("it was stuck for six days")
+			
+			
+		case 'ufos are real':
+			print('manure')
+			print("no more horses")
+			break
+		
+				
+		default:
+			print('one walking quack d')
+			print("walking geese")
+			break
+}
+'''
+#endswitch(sw)
+
+
+#clever('12')
+
+#number test faster just uncomment it and above comment endswitch(sw)#sw ='''
+
+	
+#sw = 
+'''
+	switch(exp) {  
+		case 1 thru 5:
+			print("where\'s the dog house!")
+			print('first prize')
+			print('you block head Charlie Brown')
+			break
+
+		case 6 to 10:
+			print('kangaroo hop hop!')
+			print('taught me how to write code')
+			break
+			
+		 
+		case 11 to 15:
+			print('mocha blast')
+			print('== 31 flavors===')
+			
+
+			
+
+		case 16 to 20:
+			print('ski fast in the powder')
+			print('sweet powder snow, lovely snow')
+			
+			
+		case 21:
+			print('Heavenly valley')
+			print('big snow flakes there and moggles')
+			
+			
+			
+		case 26:
+			print('oh yeah')
+			print('tahoe here I come')
+			break
+			
+		default:
+			print('six walking duck de fa ul t')
+			print('flying geese')
+            break
+}
+'''
+#endswitch(sw)
+
+
+
+    
+    
+    
+#identical copy to see what happens     
