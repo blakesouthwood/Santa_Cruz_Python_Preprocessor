f68d7c8a5fee207af9aad9835c1389f74fea6dda
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2021-12-14 11:10:22 -0800

    Add files via upload

--- frosty.py
+++ frosty.py
@@ -1,15 +1,13 @@
-###=== importing files ===================
-from woodstock import *
-from Linus import * 
-from snoopy_doghouse import * 
-from tron3 import *
-#from ready_player_one import * 
-#from drivethru import *
+###==== importing files ===================
+from woodstock import *          #  1,845  lines of code
+from Linus import *              # 27,105  lines of code
+from snoopy_doghouse import *    # 12,329  lines of code
+from tron3 import *              #    397  lines of code
 
 ####====importing lists ====================
-from Linus import switch_numbers_to_transfer
-from snoopy_doghouse import output #list
-from tron3 import topvalue #list
+from Linus import switch_numbers_to_transfer #list
+from snoopy_doghouse import output           #list
+from tron3 import topvalue                   #list
 #############################################
 #from Linus import switch_numbers_to_transfer# alist
 # december 13th, 2021 Monday 10;35 am Morgan Hill Starbucks, California
@@ -32,7 +30,7 @@ red_robin ='''
 				case 'blable':
 					print("do something")
 					####################
-					switch(exp){ # 15  #second level deep          Level 2    
+					switch(exp){   
 						case 'tahoe':
 							print("do something")
 							print("yep")
@@ -40,7 +38,7 @@ red_robin ='''
 						case 'fallen leaf lake':
 							print("nice")
 							####################
-							switch(exp){ #23  #third level deep   Level 3    
+							switch(exp){   
 								case 'tahoe':
 									print("do something")
 									print("yep")
@@ -50,7 +48,7 @@ red_robin ='''
 									break
 								default:
 									print("we are done here")
-							endswitch #33
+							endswitch 
 							#############
 							break
 						default:
@@ -64,13 +62,13 @@ red_robin ='''
 					break
 				default:
 					print("we are done here")
-			endswitch #47
+			endswitch 
 			exp = 3
-			switch(exp){ #49 #first level deep                   Level 1
+			switch(exp){ 
 				case 'burger':
 					print("do something")
 					####################
-					switch(exp){  #53  second level deep          Level 2    
+					switch(exp){     
 						case 'fishy':
 							print("do something")
 							print("yep")
@@ -90,7 +88,7 @@ red_robin ='''
 					break
 				default:
 					print("we are done here")
-			endswitch #73
+			endswitch 
 
 			##############
 			print('taught me how to write code')
@@ -127,44 +125,44 @@ inputstring =''
 #output combined python switch methods ready to be executed.
 
 
-
 #https://www.youtube.com/watch?v=X0Hyxq7iOwI
-
-
-
+#reducing lines for stage 3
+def show_generated_code():
+    print("show generated python switch methods code")
+    for line in toocool[0].splitlines():
+      print(line)
+      
 def stage(x):
     print("#=== STAGE "+str(x)+ "==")
 
+#reducing lines of code for  stage 2
+def split_up_switches_and_prepare_for_parser(inputstring):
+   split_up_big_string_into_nested_switches(inputstring);stage(2);  
+   waterfall_chain_methods_gold_master(); stage(2);  
+
 colorfilm=[]
 silver=''
+##==================================================================
+##  create_generated_python_switch_methods_concatted(inputstring):
+##===================================================================
 def create_generated_python_switch_methods_concatted(inputstring):
-   ##==============================================
-   # stage 1 woodstock.py
-   print("stage 1 called in frosty")
+   ##==============stage 1==uses woodstock.py==============================
+   print("stage 1 called in frosty=== ") #pairs of switch and endswitch by tab depth
    create_list_of_pairs_now(inputstring);stage(1); 
-   ##=================================================
-   # stage 2 Linus
-   print("stage 2 called in frosty")
-   split_up_big_string_into_nested_switches(inputstring);stage(2);        # stage 2 Linus.py  
-   waterfall_chain_methods_gold_master(); stage(2);  
-   ##========================================
-   #stage 3 snoopy_doghouse.py   
-   print("stage 3 called in frosty ==")
-   create_def_switch_methods_concatted_together_in_one_string();stage(3); # stage 3 snoopy_doghouse.py
-   #print("length of stnaford",len(stanford))
-   print("--dec 14th ---genersted docstring  python code-----")
-   for line in toocool[0].splitlines():
-      print(line)
-    ##========================================
-   #stage 4 tron3.py   
-   print("====stage 4 frosty====")
-   print("calling clever_cat() from frosty now to concat the")
-   print("dynamically generated python switch cases that look like javascript")
-   topvalue[0]='1'
-   print('here we go')
-   clever_cat(); stage(4) #this triggers indirectly the exec(string) of dynamic python!!
+   ##=============stage 2===uses Linus.py=================================
+   print("stage 2 called in frosty=== ") #get each switch case body 
+   split_up_switches_and_prepare_for_parser(inputstring)
+   ##=============stage 3====uses snoopy_doghouse.py=======================
+   print("stage 3 called in frosty=== ")  # stage 3 snoopy_doghouse.py
+   create_def_switch_methods_concatted_together_in_one_string();stage(3);
+   show_generated_code()
+   print("--dec 14th -2021--generated docstring  python switch case code methods-----")
+   ##============stage 4====uses tron3.py========================
+   print("stage 4 called in frosty== concat the dynamically generated python= ",topvalue[0])
+   clever_cat(); stage(4) #this concats the python switch methods and exec(code)
 
 
+topvalue[0]='1'
 inputstring = red_robin    
 create_generated_python_switch_methods_concatted(inputstring)    
     
