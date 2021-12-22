e0c4dbf6f12fd47d4c107039c790755de8123ee0
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2021-12-22 08:18:33 -0800

    Update angel_falls.py

--- angel_falls.py
+++ angel_falls.py
@@ -1,3 +1,12 @@
+Dec 22nd, 2021 8:15 am 
+testing.
+I have the code for nested switches in one file module now
+I am slowly hooking the pieces together.
+I am testing each section independently from the running code as I gradually connect each piece together.
+I have canned hard coded data that works and now I am replacing each section with the running code
+by turning on each new module which are each running indepently for testing purposes for quality control.
+So I am slowly cascading down works tip toing down thru the flow of control. 
+
 Dec 21st, 2021 8:17 am PST Morgan Hill, California
 
 About to connect the two last pieces together.
