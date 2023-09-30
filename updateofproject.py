2f736da91b289d88311f9c6b98edc063df1a3920
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2023-09-29 18:48:52 -0700

    I have been playing with Chat GPT and Google Bard
    I will minimize and refactor the code and have pseudocode generated for each function.
    I will also retest each function afterward and the whole project. This way people will
    be able to have more fun.  I will also add way more methods to make the code as
    minimal as possible and as human-readable as possible.

---
+++ updateofproject.py
@@ -0,0 +1,8 @@
+Fri Sep 29th, 2023 Silicon Valley, California
+
+I will refactor the code and totally comment it
+using Chat GPT and Google Bard to make it more
+beautiful. I will also have pseudocode written for
+each function.
+
+I will work on this project this week. 
